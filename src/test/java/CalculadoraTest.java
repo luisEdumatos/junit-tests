5d8998dc42ad7cf3b91c12b1dca041fec39a4d1e
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-28 14:46:28 -0300

    finished tests for calculator

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,2 +1,80 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CalculadoraTest {
+    static Calculadora calculadora;
+
+    @Test
+    public void testaSoma() {
+        Calculadora myCalc = new Calculadora();
+
+        boolean temErro = false;
+        if (myCalc.soma(2, 2) != 4) {
+            temErro = true;
+        }
+        if (myCalc.soma(2, -2) != 0) {
+            temErro = true;
+        }
+        if (myCalc.soma(-2, 2) != 0) {
+            temErro = true;
+        }
+        if (myCalc.soma(-2, -2) != -4) {
+            temErro = true;
+        }
+        if (myCalc.soma(0, 0) != 0) {
+            temErro = true;
+        }
+        if (temErro) {
+            fail("Houve um erro na validação da soma.");
+        }
+    }
+
+    @Test
+    public void deveResultarQuatroAoSomarDoisEDois() {
+        // Calculadora calculadora = new Calculadora();
+        assertEquals(4.0, calculadora.soma(2, 2));
+    }
+
+    @Test
+    public void deveResultarZeroAoSomarDoisEMenosDois() {
+        // Calculadora calculadora = new Calculadora();
+        assertEquals(0.0, calculadora.soma(2, -2));
+    }
+
+    @BeforeAll
+    public static void setup() {
+        calculadora = new Calculadora();
+    }
+    // Outros: @BeforeEach, @AfterEach, @AfterAll
+
+    @DisplayName("Valida múltiplas somas com informações em CSV")
+    @ParameterizedTest
+    @CsvSource({ "1.0, 1.0, 2.0", "2.0, 3.0, 5.0" })
+    void validaMultiplasSomasCSV(double parcela1, double parcela2, double resultadoEsperado) {
+        assertEquals(resultadoEsperado, calculadora.soma(parcela1, parcela2));
+    }
+
+    @DisplayName("Valida múltiplas somas com informações em arquivo CSV")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void validaMultiplasSomasArqCSV(double parcela1, double parcela2, double resultadoEsperado) {
+        assertEquals(resultadoEsperado, calculadora.soma(parcela1, parcela2));
+    }
+
+    @Test
+    public void testaExcecao() {
+        assertThrows(ArithmeticException.class, () -> {
+            int retorno = 4 / 0;
+            System.out.println(retorno);
+        });
+    }
 }
